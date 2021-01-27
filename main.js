ec7e0512c3555247ece159ee8237b24865504dfa
Duncan <sean.d.purcell@gmail.com>
2021-01-27 07:36:23 -0800

    prep tabel for web-portfolio

--- main.js
+++ main.js
@@ -37,7 +37,8 @@ class Player {
 
 class Deck {
   constructor(){
-    const _standardDeck = ["2C", "3C", "4C", "5C", "6C", "7C", "8C", "9C", "10C", "JC", "QC", "KC", "AC" 
+    const _standardDeck = 
+    ["2C", "3C", "4C", "5C", "6C", "7C", "8C", "9C", "10C", "JC", "QC", "KC", "AC" 
       ,"2S", "3S", "4S", "5S", "6S", "7S", "8S", "9S", "10S", "JS", "QS", "KS", "AS" 
       ,"2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H", "10H", "JH", "QH", "KH", "AH" 
       ,"2D", "3D", "4D", "5D", "6D", "7D", "8D", "9D", "10D", "JD", "QD", "KD", "AD" 
@@ -147,14 +148,14 @@ const Interface = (function () {
 
     const imgArr = 
     [
-      [`./img/${suit}.svg`, "./img/Card_back_01.svg"],
+      [`${suit}.svg`, "Card_back_01.svg"],
       ["cardFront", "cardBack"]
     ];
 
     let i = 0;
     while(i < imgArr[0].length){
       const img = document.createElement("img");
-      img.src = imgArr[0][i];
+      img.src = `http://${location.host}/projects/blackJack/img/${imgArr[0][i]}`;
       img.className = imgArr[1][i];
       card.appendChild(img);
 
@@ -398,25 +399,27 @@ const GameDirector = (function(){
       if (result == "Win"){
         let bet = (playerBets[i] * 2);
         _players[1].wallet += bet;
-        winnings += ` Hand ${i + 1}: Wins $${playerBets[i]}.00 `;
+        winnings += `Your Hand 0${i + 1}: Wins $${playerBets[i]}.00 `;
       }
       else if (result == "Push"){
         _players[1].wallet += (playerBets[i]);
-        winnings += ` Hand ${i + 1}: Pushed `;
+        winnings += `Your Hand 0${i + 1}: Pushed `;
       }
       else if (result == "BlackJack"){
         let bet = (playerBets[i] * 2.5);
         _players[1].wallet += bet;
-        winnings += ` Hand ${i + 1}: Wins $${playerBets[i] * 1.5} `;
+        winnings += `Your Hand 0${i + 1}: Wins $${playerBets[i] * 1.5} `;
       }
       else if (result == "Lose"){
         let bet = (playerBets[i]);
-        winnings += ` Hand ${i + 1}: Loses $${bet}.00 `;
+        winnings += `Your Hand 0${i + 1}: Loses $${bet}.00 `;
       }
       playerBets[i] = 0;
       //set player individual hand to show outcome;
       if (result != undefined) Interface.setHandBlocker(result, i);
-    }    
+    }
+
+    winnings += 'Click Here For Another Hand.'
     Interface.setWallet(_players[1].wallet);
     document.getElementById("resultsDisplay").addEventListener('click', _clearTable);
     Interface.showRslt(winnings);
